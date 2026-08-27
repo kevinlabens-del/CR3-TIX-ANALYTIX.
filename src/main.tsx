a07a7babf13ca56bf42c1a9879d0de8f9d50868f
kevinlabens-del <kevin.labens@gmail.com>
2026-08-27 07:09:47 +0200

    feat: intégrer le mode d'emploi dans CR3@TIX ANALYTIX
    * feat: add in-app Analytix user guide
    
    * style: add responsive in-app guide layout
    
    * chore: mark guide feature branch
    
    * chore: prepare guide integration
    
    * docs: note guide integration
    
    * docs: add guide integration note
    
    * feat: expose guide through floating in-app help
    
    * style: add floating help button and guide modal
    
    * feat: mount integrated user guide
    
    * chore: remove temporary guide marker
    
    * chore: remove temporary guide file
    
    * chore: remove temporary guide note
    
    * chore: remove temporary guide documentation

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import GuideLauncher from './Guide';
 import './styles.css';
+import './guide.css';
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
+createRoot(document.getElementById('root')!).render(<StrictMode><App/><GuideLauncher/></StrictMode>);
 if('serviceWorker' in navigator)window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js').catch(()=>undefined));
